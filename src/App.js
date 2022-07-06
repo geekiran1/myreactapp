c03ba8792d196113973d04765b210c24064d4751
sruthi109 <sruthi.podapati@gmail.com>
2022-07-05 22:20:54 -0400

    commit

--- src/App.js
+++ src/App.js
@@ -1,20 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-<<<<<<< HEAD
-        <h2>Testing the pull changes from server</h2>
-        <h3>Adding new codes</h3>
-=======
         <h3>Testing the pull changes from server</h3>
         <h3>Testing Radhen Khant</h3>
->>>>>>> 945ef2ca2a3fc658d645c92fa5d6b7ac65165cbc
         <p>
-          Edit <code>src/App.js</code> and save to reload. Testing branching strategy.
+          Edit <code>src/App.js</code> and save to reload. Testing branching
+          strategy.
         </p>
         <a
           className="App-link"
