9ec694f3ded91fc26492debbed2ae828df782581
Radhen <200454406@student.georgianc.on.ca>
2022-07-05 22:05:09 -0400

    radhen Commit

--- src/App.js
+++ src/App.js
@@ -9,6 +9,7 @@ function App() {
         <h2>Testing the pull changes from server</h2>
         <h4>Adding new codes, tag changed at server</h4>
         <h3>Adding new codes. Changed in local</h3>
+        <h3>Adding new codes. By Radhen</h3>
         <p>
           Edit <code>src/App.js</code> and save to reload. Testing branching strategy.
         </p>
